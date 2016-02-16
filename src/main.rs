5cb6d6223d26384f3c77612dd7c9ea49c7980083
Joe Lloyd <josephglloyd@gmail.com>
2016-02-16 21:28:25 +0000

    Set up five philosophers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let soren = Philosopher::new("Soren Kierkegaard");
+    let gary = Philosopher::new("Gary Grosvenor");
+    let jennifer = Philosopher::new("Jennifer Hotfire");
+    let hywel = Philosopher::new("Hywel Price");
+    let helen = Philosopher::new("Helen Pringle");
+
 }
