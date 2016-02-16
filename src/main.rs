a17c0fb78352d0f91dcf48e04b4fa873f07ea0cc
Joe Lloyd <josephglloyd@gmail.com>
2016-02-16 22:12:11 +0000

    5 philosophers use 4 forks to eat

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,58 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::{Mutex, Arc};
 
 struct Philosopher {
     name: String,
+    left: usize,
+    right: usize,
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right: right,
         }
     }
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep(Duration::from_millis(150));
+        let _right = table.forks[self.right].lock().unwrap();
+
         println!("{} has started eating", self.name);
         thread::sleep(Duration::from_millis(1000));
         println!("{} is done eating", self.name);
     }
 }
 
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
 
 fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
     let philosophers = vec![
-        Philosopher::new("Soren Kierkegaard"),
-        Philosopher::new("Gary Grosvenor"),
-        Philosopher::new("Jennifer Hotfire"),
-        Philosopher::new("Hywel Price"),
-        Philosopher::new("Helen Pringle"),
+        Philosopher::new("Soren Kierkegaard", 0, 1),
+        Philosopher::new("Gary Grosvenor", 1, 2),
+        Philosopher::new("Jennifer Hotfire", 2, 3),
+        Philosopher::new("Hywel Price", 3, 4),
+        Philosopher::new("Helen Pringle", 0, 4),
     ];
 
     let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+
         thread::spawn(move || {
-            p.eat();
+            p.eat(&table);
         })
     }).collect();
 
